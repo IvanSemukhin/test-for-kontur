33e89f62899a6432f003d983365f7cdb4d5a52b3
ivan <rubberman88@rabler.ru>
2017-10-06 14:15:13 +0500

    add functionality and complet first cycle

--- func.py
+++ func.py
@@ -1,19 +1,21 @@
 import subprocess
+import os
+import pickle
 
 
 def init_script():
-    param = dict()
+    init_param = dict()
     try:
         ini_file = open("ini", "r")
         tmp = ini_file.read()
         tmp = tmp.split()
         for i in range(0, len(tmp) - 1, 2):
             if tmp[i] == "URL":
-                param[tmp[i]] = tmp[i + 1]
+                init_param[tmp[i]] = tmp[i + 1]
             if tmp[i] == "DELAY":
-                param[tmp[i]] = tmp[i + 1]
+                init_param[tmp[i]] = tmp[i + 1]
             if tmp[i] == "EMAIL":
-                param[tmp[i]] = tmp[i + 1]
+                init_param[tmp[i]] = tmp[i + 1]
         print("Параметры считаны из файла[" + ini_file.name + "]")
         ini_file.close()
     except FileNotFoundError:
@@ -22,23 +24,26 @@ def init_script():
 
         url = "https://gitlab.com/kontur-tasks/trytobuild.git"
         # url = "https://github.com/IvanSemukhin/testrepo.git"
-        param["URL"] = url
-        print(param["URL"])
+        init_param["URL"] = url
+        print(init_param["URL"])
         ini_file.write("URL " + url + "\n")
 
         delay = "60"
-        param["DELAY"] = delay
-        print(param["DELAY"])
+        init_param["DELAY"] = delay
+        print(init_param["DELAY"])
         ini_file.write("DELAY " + delay + "\n")
 
         email = "test@mail.ru"
-        param["EMAIL"] = email
-        print(param["EMAIL"])
+        init_param["EMAIL"] = email
+        print(init_param["EMAIL"])
         ini_file.write("EMAIL " + email + "\n")
 
         print("\nФайл [" + ini_file.name + "] записан со значениями по умолчанию\n")
         ini_file.close()
-    return param
+    return init_param
+
+
+param = init_script()
 
 
 def clone(remote):
@@ -66,8 +71,7 @@ def clone(remote):
             return result[left:right]
 
         if result.find("Could not resolve host") != -1:
-            print("Send EMAIL: From function clone() ", result)
-            exit(1)
+            send_mail(clone.__name__, param["EMAIL"], result)
 
 
 def remote_branch(name_repo):
@@ -98,8 +102,7 @@ def remote_branch(name_repo):
 
     except subprocess.CalledProcessError as e:
         result = str(e.output.decode('UTF-8'))  # Получить данные вывода исключения
-        print("Send EMAIL: From function remote_branch() ", result)
-        exit(1)
+        send_mail(remote_branch.__name__, param["EMAIL"], result)
 
 
 def add_to_track(branches, name_repo):
@@ -121,7 +124,8 @@ def add_to_track(branches, name_repo):
             if result.find("A branch named '" + name_branch + "' already exists.") != -1:
                 continue
             else:
-                print("Send EMAIL: From function add_to_track() ", result)
+                # print("Send EMAIL: From function add_to_track() ", result)
+                send_mail(add_to_track.__name__, param["EMAIL"], result)
 
 
 def pull(name_repo):
@@ -132,16 +136,15 @@ def pull(name_repo):
         return result
     except subprocess.CalledProcessError as e:
         result = str(e.output.decode('UTF-8'))  # Получить данные вывода исключения
-        print("Send EMAIL: From function pull() ", result)
-        exit(1)
+        send_mail(pull.__name__, param["EMAIL"], result)
 
 
 def log(branches, name_repo):
     """
     Получает список веток и имя каталога реозитория
     Пробегает по всем веткам(выполняет checkout в каждую ветку)
-    Командой git получает лог
-    Парсит лог на кортеж (ХЭШ_коммита, имя_автора)
+    Командой git log получает инфо о ветке
+    Парсит инфо на кортеж (ХЭШ_коммита, имя_автора)
     В случае успеха возвращает словарь (имя_ветки: кортеж)
     Иначе завершает программу
     """
@@ -160,20 +163,19 @@ def log(branches, name_repo):
                 heads[elem] = commit_and_author
         except subprocess.CalledProcessError as e:
             result = str(e.output.decode('UTF-8'))  # Получить данные вывода исключения
-            print("Send EMAIL: From function log() ", result)
-            exit(1)
+            send_mail(log.__name__, param["EMAIL"], result)
     return heads
 
 
 def checkout(branch, name_repo):
+    # print("branch form checkout = ", branch)
     try:
         # subprocess.check_output(["git", "--git-dir", name_repo + "/.git", "checkout", branch],
         #                         stderr=subprocess.STDOUT)
         subprocess.check_output(["git", "checkout", branch], stderr=subprocess.STDOUT, cwd=name_repo)
     except subprocess.CalledProcessError as e:
         result = str(e.output.decode('UTF-8'))  # Получить данные вывода исключения
-        print("Send EMAIL: From function checkout() ", result)
-        exit(1)
+        send_mail(checkout.__name__, param["EMAIL"], result)
 
 
 def diff(heads, current_heads):
@@ -187,8 +189,121 @@ def diff(heads, current_heads):
     return modify_branch
 
 
-def build(branches):
-    print("CALL BUILD:")
-    print(branches)
-    for elem in branches:
-        print(elem)
+def build(branches, name_repo, heads):
+    # print("CALL BUILD:")
+    # print(branches)
+    for elem in branches:   # каждый элемент: origin/master
+        # print(elem)
+        name_branch = elem.split("/")[1]
+        checkout(name_branch, name_repo)
+        try:
+            # print("OLOLO", r"/p:TargetFrameworkVersion=\"v4.5\"")
+            # input()
+            # result = subprocess.check_call("mono nuget.exe", stderr=subprocess.STDOUT, shell=True)
+            result = subprocess.check_output("xbuild /p:TargetFrameworkVersion=\"v4.5\"",
+                                             stderr=subprocess.STDOUT,
+                                             cwd=name_repo,
+                                             shell=True)
+            result = str(result)
+            if result.find("Build succeeded.") == -1:
+                raise subprocess.CalledProcessError(1, "xbuild /p:TargetFrameworkVersion=\"v4.5\"", result)
+            else:
+                print("Build succeeded.")
+                result = result[result.find("Target Build:"):]
+                left = result.find(name_repo)
+                right = result.find(".csproj")
+                name_app = result[left:right].split("/")[1]
+                description = execute(name_repo, name_app)
+                version = get_version(name_repo)
+                commit_hash = heads[elem][0]
+                author = heads[elem][1]
+                author.rstrip()
+                pack(get_id(), version, description, commit_hash, author, name_app, name_repo)
+        except subprocess.CalledProcessError as e:
+            result = str(e.output)  # Получить данные вывода исключения
+            send_mail(build.__name__, param["EMAIL"], result)
+
+
+def execute(name_repo, name_app):
+    try:
+        result = subprocess.check_output("mono " + name_repo + "/bin/Debug/" + name_app + ".exe",
+                                         stderr=subprocess.STDOUT,
+                                         shell=True)
+        # result = str(result)
+        result = result.decode('UTF-8')
+        # print(result)
+    except subprocess.CalledProcessError as e:
+        result = str(e.output)  # Получить данные вывода исключения
+        send_mail(execute.__name__, param["EMAIL"], result)
+    return result
+
+
+def pack(identifier, version, description, commit_hash, author, name_app, name_repo):
+    # print("CALL pack")
+    # print(identifier, version, description, commit_hash, author, name_app)
+    try:
+        subprocess.check_output("mono ../nuget.exe spec -Force",
+                                cwd=name_repo,
+                                stderr=subprocess.STDOUT,
+                                shell=True)
+        print("Created nuspec successfully")
+
+    except subprocess.CalledProcessError as e:
+        result = str(e.output)
+        send_mail(pack.__name__, param["EMAIL"], result)
+
+    file = open(name_repo + "/" + name_app + ".nuspec", "r")
+    text = file.read()
+    file.close()
+    text = text.replace("$id$", identifier, 1)
+    text = text.replace("$version$", version, 1)
+    text = text.replace("$title$", name_app, 1)
+    text = text.replace("$author$", author, 2)
+    text = text.replace("$description$", description + " " + commit_hash, 1)
+
+    file = open(name_repo + "/" + name_app + ".nuspec", "w")
+    file.write(text)
+    file.close()
+
+    try:
+        subprocess.check_output("mono ../nuget.exe pack " + name_app + ".nuspec -Verbosity detailed",
+                                cwd=name_repo,
+                                stderr=subprocess.STDOUT,
+                                shell=True)
+        print("Successfully created package")
+    except subprocess.CalledProcessError as e:
+        result = str(e.output)
+        send_mail(pack.__name__, param["EMAIL"], result)
+
+
+def get_version(name_repo):
+    for line in open(name_repo + "/Properties/AssemblyInfo.cs", "r"):
+        if line.rfind("AssemblyVersion") != -1 and "*" not in line:
+            return line[line.find("\"") + 1:line.rfind("\"")]
+
+
+def get_id():
+    id_file_name = ".id.bin"
+    if os.path.exists(id_file_name):
+        to_load = open(id_file_name, 'rb')
+        app_id = pickle.load(to_load)
+        to_load.close()
+
+        app_id = str(int(app_id)+1)
+
+        output = open(id_file_name, 'wb')
+        pickle.dump(app_id, output, 4)
+        output.close()
+    else:
+        output = open(id_file_name, 'wb')
+        app_id = "1"
+        pickle.dump(app_id, output, 4)
+        output.close()
+    return app_id
+
+
+def send_mail(name_fun, email, message):
+    print("Send EMAIL to", email)
+    print("From function", name_fun)
+    print("Message:", message)
+    # exit(1)
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import time
-import os
-import pickle
+# import os
+# import pickle
 from func import *
 
-param = init_script()
 first = True
 current_heads = ""
+heads_file_name = ".heads.bin"
 while True:
     if first:
-        first = False
         print("First START:")
         name_repo = clone(param["URL"])
+        # print(param)
         # print("name_repo= " + name_repo)
 
         branches = remote_branch(name_repo)
@@ -18,21 +18,24 @@ while True:
 
         add_to_track(branches, name_repo)
 
-        if os.path.exists("heads.bin"):
-            to_load = open('heads.bin', 'rb')
+        if os.path.exists(heads_file_name):
+            to_load = open(heads_file_name, 'rb')
             current_heads = pickle.load(to_load)
             # print("current_heads from bin = ", current_heads)
             to_load.close()
+            current_heads.clear()   # FOR DEBUG
         else:
             current_heads = log(branches, name_repo)
             # print("current_heads from func = ", current_heads)
-            output = open('heads.bin', 'wb')
+            output = open(heads_file_name, 'wb')
             pickle.dump(current_heads, output, 4)
             output.close()
 
         heads = log(branches, name_repo)
         modify_branch = diff(heads, current_heads)
-        build(modify_branch)
+        build(modify_branch, name_repo, heads)
+
+        first = False
     else:
         print("NEXT:")
         print("pull, get_diff, build, start, pack")
